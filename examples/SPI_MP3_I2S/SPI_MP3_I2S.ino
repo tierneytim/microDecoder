34d764944a1ba8918a35f15e3f1e433716981026
Tim Tierney <fiachmri@gmail.com>
2020-08-01 12:54:25 +0100

    updated I2S example

--- examples/SPI_MP3_I2S/SPI_MP3_I2S.ino
+++ examples/SPI_MP3_I2S/SPI_MP3_I2S.ino
@@ -1,7 +1,9 @@
 
 #include "SD.h"                               // input
 #include "mp3.h"                              // decoder
-#include "I2S.h"                              // output
+
+// needs I2S32 library (https://github.com/tierneytim/I2S32)
+#include "I2S32.h"                              // output
 
 mp3 MP3;                                      // MP3 class
 pcm audio;                                    // Generic audio data class 
@@ -9,10 +11,11 @@ I2S i2s;                                      // I2S class used for output
  
 void setup() {
   SD.begin();                                 // Start SD interface
-  File file = SD.open("/owa3.mp3");           // Open the desired file
+  File file = SD.open("/cc.mp3");           // Open the desired file
   
   MP3.begin(file);                            // pass the file to the MP3 Class
-     
+  
+  // bclk, din, wsel
   i2s.begin(26,25,27);                        // Set pins for I2S 
   i2s.setSampleRate(MP3.Fs);                  // Set sample rate
 }
